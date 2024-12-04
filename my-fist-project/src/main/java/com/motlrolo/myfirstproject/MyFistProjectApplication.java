0d8758fe2ba0107de98dab48da9ee5d5a38bb681
Juda <judacarrillot@gmail.com>
2024-12-03 22:53:07 -0500

    Start first project

---
+++ my-fist-project/src/main/java/com/motlrolo/myfirstproject/MyFistProjectApplication.java
@@ -0,0 +1,13 @@
+package com.motlrolo.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFistProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFistProjectApplication.class, args);
+    }
+
+}
---
+++ my-fist-project/src/test/java/com/motlrolo/myfirstproject/MyFistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motlrolo.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFistProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
